4e7e0e4077b69d612fb0a89b1979573d04ed4ac0
Farhadsb98 <124209727+Farhadsb98@users.noreply.github.com>
2023-03-26 10:03:05 +0400

    pc

--- Taks 6/Program.cs
+++ Taks 6/Program.cs
@@ -1,157 +1,160 @@
-﻿namespace Taks_6
+﻿using System.Net.Mail;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Taks_6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-        #region task 1
-        // l1:
-        // Console.Write("C ededini daxil edin:");
-        // string strvalue=Console.ReadLine();
-        // bool eded = int.TryParse(strvalue, out int value);
-        // if (!eded)
-        // {
-        //     goto l1;
-        // }
+            #region task 1
+            // l1:
+            // Console.Write("C ededini daxil edin:");
+            // string strvalue=Console.ReadLine();
+            // bool eded = int.TryParse(strvalue, out int value);
+            // if (!eded)
+            // {
+            //     goto l1;
+            // }
 
-        //l2:
-        // Console.Write("Massivin uzunlugunu daxil edin");
-        // string struzunluq=Console.ReadLine();
-        // bool uzun = int.TryParse(struzunluq, out int uzunluq);
-        // if (!uzun)
-        // {
-        //     goto l2;
-        // }
-        // int[] a=new int[uzunluq];
-        // for (int i = 0; i <a.Length; i++)
-        // {
-        //     l3:
-        //     Console.Write($"[{i+1}]=");
-        //     string strarray = Console.ReadLine();
-        //     bool tr = int.TryParse(strarray, out int array);
-        //     if (!tr)
-        //     {
-        //         goto l3;
-        //     }
-        //     a[i] = array;
-
-
-        // }
-        // for (int i = 0; i < a.Length; i++)
-        // {
-        //     if (a[i] < value)
-        //     {
-        //         Console.WriteLine($"[{i}]={a[i]}");
-        //     }
-        // }
+            //l2:
+            // Console.Write("Massivin uzunlugunu daxil edin");
+            // string struzunluq=Console.ReadLine();
+            // bool uzun = int.TryParse(struzunluq, out int uzunluq);
+            // if (!uzun)
+            // {
+            //     goto l2;
+            // }
+            // int[] a=new int[uzunluq];
+            // for (int i = 0; i <a.Length; i++)
+            // {
+            //     l3:
+            //     Console.Write($"[{i+1}]=");
+            //     string strarray = Console.ReadLine();
+            //     bool tr = int.TryParse(strarray, out int array);
+            //     if (!tr)
+            //     {
+            //         goto l3;
+            //     }
+            //     a[i] = array;
 
 
-        #endregion
-
-        #region task2
-        //l1:
-        //    Console.Write("C ededini daxil edin:");
-        //    string strvalue = Console.ReadLine();
-        //    bool eded = int.TryParse(strvalue, out int value);
-        //    if (!eded)
-        //    {
-        //        goto l1;
-        //    }
-
-        //l2:
-        //    Console.Write("Massivin uzunlugunu daxil edin");
-        //    string struzunluq = Console.ReadLine();
-        //    bool uzun = int.TryParse(struzunluq, out int uzunluq);
-        //    if (!uzun)
-        //    {
-        //        goto l2;
-        //    }
-        //    int[] a = new int[uzunluq];
-        //    for (int i = 0; i < a.Length; i++)
-        //    {
-        //    l3:
-        //        Console.Write($"[{i + 1}]=");
-        //        string strarray = Console.ReadLine();
-        //        bool tr = int.TryParse(strarray, out int array);
-        //        if (!tr)
-        //        {
-        //            goto l3;
-        //        }
-        //        a[i] = array;
+            // }
+            // for (int i = 0; i < a.Length; i++)
+            // {
+            //     if (a[i] < value)
+            //     {
+            //         Console.WriteLine($"[{i}]={a[i]}");
+            //     }
+            // }
 
 
-        //    }
-        //    int count = 0;
-        //    int silsile = 0;
+            #endregion
 
-        //    for (int i = 0; i < a.Length; i++)
-        //    {
-        //        if (a[i] < value)
-        //        {
-        //            silsile = silsile + a[i];
-        //            count++;
+            #region task2
+            //l1:
+            //    Console.Write("C ededini daxil edin:");
+            //    string strvalue = Console.ReadLine();
+            //    bool eded = int.TryParse(strvalue, out int value);
+            //    if (!eded)
+            //    {
+            //        goto l1;
+            //    }
 
-
-        //        }
-        //    }
-        //   decimal orta = (decimal)silsile / count;
-        //    Console.WriteLine(orta);
-        #endregion
-
-        #region task3
-        //l1:
-        //    Console.Write("C ededini daxil edin:");
-        //    string strvalue = Console.ReadLine();
-        //    bool eded = int.TryParse(strvalue, out int value);
-        //    if (!eded)
-        //    {
-        //        goto l1;
-        //    }
-
-        //l2:
-        //    Console.Write("Massivin uzunlugunu daxil edin");
-        //    string struzunluq = Console.ReadLine();
-        //    bool uzun = int.TryParse(struzunluq, out int uzunluq);
-        //    if (!uzun)
-        //    {
-        //        goto l2;
-        //    }
-        //    int[] a = new int[uzunluq];
-        //    for (int i = 0; i < a.Length; i++)
-        //    {
-        //    l3:
-        //        Console.Write($"[{i + 1}]=");
-        //        string strarray = Console.ReadLine();
-        //        bool tr = int.TryParse(strarray, out int array);
-        //        if (!tr)
-        //        {
-        //            goto l3;
-        //        }
-        //        a[i] = array;
+            //l2:
+            //    Console.Write("Massivin uzunlugunu daxil edin");
+            //    string struzunluq = Console.ReadLine();
+            //    bool uzun = int.TryParse(struzunluq, out int uzunluq);
+            //    if (!uzun)
+            //    {
+            //        goto l2;
+            //    }
+            //    int[] a = new int[uzunluq];
+            //    for (int i = 0; i < a.Length; i++)
+            //    {
+            //    l3:
+            //        Console.Write($"[{i + 1}]=");
+            //        string strarray = Console.ReadLine();
+            //        bool tr = int.TryParse(strarray, out int array);
+            //        if (!tr)
+            //        {
+            //            goto l3;
+            //        }
+            //        a[i] = array;
 
 
-        //    }
-        //    double final = 0;
-        //    int count = 0;
-        //    int silsile = 1;
-        //    for (int i = 0; i < a.Length; i++)
-        //    {
-        //        if (a[i] > value)
-        //        {
-        //            silsile = silsile * a[i];
-        //            count++;
+            //    }
+            //    int count = 0;
+            //    int silsile = 0;
 
-        //        }
-
-        //    }
-        //    double count2 = 1.0 / count;
-        //    final = Math.Pow(silsile, count2);
-
-        //    Console.WriteLine(final);
+            //    for (int i = 0; i < a.Length; i++)
+            //    {
+            //        if (a[i] < value)
+            //        {
+            //            silsile = silsile + a[i];
+            //            count++;
 
 
+            //        }
+            //    }
+            //   decimal orta = (decimal)silsile / count;
+            //    Console.WriteLine(orta);
+            #endregion
 
-        //    #endregion
+            #region task3
+            //l1:
+            //    Console.Write("C ededini daxil edin:");
+            //    string strvalue = Console.ReadLine();
+            //    bool eded = int.TryParse(strvalue, out int value);
+            //    if (!eded)
+            //    {
+            //        goto l1;
+            //    }
+
+            //l2:
+            //    Console.Write("Massivin uzunlugunu daxil edin");
+            //    string struzunluq = Console.ReadLine();
+            //    bool uzun = int.TryParse(struzunluq, out int uzunluq);
+            //    if (!uzun)
+            //    {
+            //        goto l2;
+            //    }
+            //    int[] a = new int[uzunluq];
+            //    for (int i = 0; i < a.Length; i++)
+            //    {
+            //    l3:
+            //        Console.Write($"[{i + 1}]=");
+            //        string strarray = Console.ReadLine();
+            //        bool tr = int.TryParse(strarray, out int array);
+            //        if (!tr)
+            //        {
+            //            goto l3;
+            //        }
+            //        a[i] = array;
+
+
+            //    }
+            //    double final = 0;
+            //    int count = 0;
+            //    int silsile = 1;
+            //    for (int i = 0; i < a.Length; i++)
+            //    {
+            //        if (a[i] > value)
+            //        {
+            //            silsile = silsile * a[i];
+            //            count++;
+
+            //        }
+
+            //    }
+            //    double count2 = 1.0 / count;
+            //    final = Math.Pow(silsile, count2);
+
+            //    Console.WriteLine(final);
+
+
+
+            #endregion
 
             #region task4 
             //l1:
@@ -249,20 +252,160 @@
             //    Console.WriteLine(enboyuk);
             #endregion
 
-
-            #region task10
+            #region task6
             //l1:
-            //            Console.Write("Massivin uzunlugunu daxil edin:");
-            //            string struzun=Console.ReadLine();
-            //            bool tr = int.TryParse(struzun, out int uzunluq);
-            //            if (!tr)
+            //    Console.Write("1-ci massivin uzunlugunu daxil edin");
+            //    string struzunluq = Console.ReadLine();
+            //    bool uzun = int.TryParse(struzunluq, out int uzunluq);
+            //    if (!uzun)
+            //    {
+            //        goto l1;
+            //    }
+            //    int[] a = new int[uzunluq];
+            //    for (int i = 0; i < a.Length; i++)
+            //    {
+            //    l2:
+            //        Console.Write($"[{i + 1}]=");
+            //        string strarray = Console.ReadLine();
+            //        bool tr = int.TryParse(strarray, out int array);
+            //        if (!tr)
+            //        {
+            //            goto l2;
+            //        }
+            //        a[i] = array;
+
+            //        int[] b = new int[uzunluq];
+
+            //    l4:
+            //        Console.Write($"[{i + 1}]=");
+            //            string strarray1 = Console.ReadLine();
+            //            bool tr1 = int.TryParse(strarray1, out int array1);
+            //            if (!tr1)
             //            {
-            //                goto l1;
+            //                goto l4;
             //            }
-            //            int[] mass=new int[uzunluq];
+            //            b[i] = array1;
+
+
+            //    if (a[i] == Math.Pow(b[i], 2))
+            //    {
+            //            Console.WriteLine($"x[{i}]={a[i]}");
+            //            Console.WriteLine($"y[{i}]={b[i]}");
+            //    }
+
+            //}
+            #endregion
+
+            #region task7
+            //l1:
+            //    Console.Write("Massivin uzunlugunu daxil edin");
+            //    string struzunluq = Console.ReadLine();
+            //    bool uzun = int.TryParse(struzunluq, out int uzunluq);
+            //    if (!uzun)
+            //    {
+            //        goto l1;
+            //    }
+            //    int[] a = new int[uzunluq];
+            //    for (int i = 0; i < a.Length; i++)
+            //    {
+            //    l3:
+            //        Console.Write($"[{i + 1}]=");
+            //        string strarray = Console.ReadLine();
+            //        bool tr = int.TryParse(strarray, out int array);
+            //        if (!tr)
+            //        {
+            //            goto l3;
+            //        }
+            //        a[i] = array;
+            //    }
+
+            //        int max = a[0];
+            //        int min = a[0];
+            //        for (int i = 0; i < a.Length; i++)
+            //        {
+            //            if (max < a[i])
+            //            {
+            //                max = a[i];
+            //            }
+            //            else if (min > a[i])
+            //            {
+            //                min = a[i];
+            //            }
+            //        }
+            //        int avarage=(min+max)/2;
+            //        double geoMan = Math.Pow(min * max, 1.0 / 2);
+            //        Console.WriteLine($"ededi orta:{avarage}; hendesi silsile:{geoMan}");
+
+
+
+
+
+
+
+
+            #endregion
+
+            #region task 8
+            int[] mass = new int[14];
+            int[] mass1 = new int[14];
+
+            for (int i = 0; i < mass.Length; i++)
+            {
+
+            l3:
+                Console.Write($"[{i + 1}]=");
+                string strarray = Console.ReadLine();
+                bool tr = int.TryParse(strarray, out int array);
+                if (!tr)
+                {
+                    goto l3;
+                }
+                mass[i] = array;
+            }
+
+            #endregion
+            #region task 10
+            //Console.Write("Massivin uzunlugun daxil edin:");  
+            //string struzun=Console.ReadLine();
+            //bool tr=int.TryParse(struzun, out int uzunluq);
+            //int[]mass=new int[uzunluq];
+            //int enkicik = 0;
+            //for (int i = 0; i < mass.Length; i++)
+            //{
+            //    Console.Write($"[{i + 1}]=");
+            //    string strarray=Console.ReadLine();
+            //    bool tru=int.TryParse(strarray, out int array);
+            //    mass[i]=array;
+            //    if (mass[i]<0)
+            //    {
+            //        if (enkicik > mass[i])
+            //        {
+            //            enkicik = mass[i];
+            //        }
+            //    }
+
+            //}
+            //Console.WriteLine(enkicik);
+            #endregion
+
+            #region task 11
+            //Console.Write("Massivin uzunlugun daxil edin:");
+            //string struzun = Console.ReadLine();
+            //bool tr = int.TryParse(struzun, out int uzunluq);
+            //int[] mass = new int[uzunluq];
+            //Random random= new Random();
+            //int enkicik = 0;
+            //for (int i = 0; i < mass.Length; i++)
+            //{
+            //    Console.Write($"[{i + 1}]=");
+
+
+            //    mass[i] = random.Next(0,101);
+            //    Array.Sort(mass);
+            //    Console.WriteLine(mass[i]);
+
+            //}
             #endregion
 
         }
-
-    }
-}
+    }   }
